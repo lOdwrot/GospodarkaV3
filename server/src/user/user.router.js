398c4b7c04f1f565b7a88f7d1bfef1ce2dd0d5a1
lodwr <l.odwrot@gmail.com>
2019-06-04 23:59:12 +0200

    Finished enpoint to get user by name ans surname

--- server/src/user/user.router.js
+++ server/src/user/user.router.js
@@ -1,10 +1,16 @@
 import {Router} from 'express'
 import * as userControler from './user.controler'
 
+// Grzesek, jbc to do parametrów z roznych czesci requesta mozesz dobrac sie tak
+// request.headers.token,
+// request.params.licenseKey,
+// request.body.newOwner,
+
 const router = Router()
 
-router.get('/login', async (req, res) => {
-    res.json(await userControler.login(request.body.mail, request.body.password))
+// Ten endpoint jbc uderza na ściezke: http://localhost:5000/user/login
+router.post('/login', async (req, res) => {
+    res.json(await userControler.login(req.body.mail, req.body.password))
 })
 
 router.get('/', async (req, res) => {
