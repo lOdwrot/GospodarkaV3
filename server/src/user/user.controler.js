f6a9187f229577a5c75effa770ff16c5438e44da
Grzegorz <grzegorz.j.pawluk@gmail.com>
2019-06-06 22:01:22 +0200

    user modifications

--- server/src/user/user.controler.js
+++ server/src/user/user.controler.js
@@ -1,4 +1,5 @@
 import {getCollectionUser} from '../database'
+import mongo from "mongodb"
 
 export const getAllUsers = async () => await getCollectionUser()
                                                 .find()
@@ -6,3 +7,22 @@ export const getAllUsers = async () => await getCollectionUser()
 
 export const login = async (mail, password) => await getCollectionUser()
                                                         .findOne({mail, password})
+
+export const getUser = async (userId) => await getCollectionUser()
+                                                       .findOne({_id: new mongo.ObjectID(userId)})
+
+export const updateUser = async (newData) => await getCollectionUser()
+                                                        .updateOne(
+                                                            {_id: new mongo.ObjectID(newData._id)},
+                                                            {$set: {...newData, _id: new mongo.ObjectID(newData._id)}}
+                                                        )
+
+export const insertUser = async (newUser) => await getCollectionUser()
+                                                            .insertOne(
+                                                                newUser
+                                                            )
+
+export const deleteUser = async (userId) => await getCollectionUser()
+                                                            .deleteOne(
+                                                                {_id: new mongo.ObjectID(userId)}
+                                                            )
