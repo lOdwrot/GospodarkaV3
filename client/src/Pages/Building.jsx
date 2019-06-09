4a40b2307b393010a22b5bf821746c685473dc22
lodwr <l.odwrot@gmail.com>
2019-06-09 22:58:58 +0200

    Added router params, added logout button

--- client/src/Pages/Building.jsx
+++ client/src/Pages/Building.jsx
@@ -3,12 +3,13 @@ import WorkerAssignment from '../components/WorkersAssignment/WorkerAssignment';
 import MachinesAssignment from '../components/MachinesAssignment/MachinesAssignment';
 import BuildingDetails from '../components/BuildingDetails/BuildingDetails';
 
-export default () => {
+export default (props) => {
+    const projectId = props.match.params.id;
     return (
         <div style={{display: 'flex'}}>
-            <WorkerAssignment/>
-            <MachinesAssignment/>
-            <BuildingDetails/>
+            <WorkerAssignment projectId={projectId}/>
+            <MachinesAssignment projectId={projectId}/>
+            <BuildingDetails projectId={projectId}/>
         </div>
     )
 }
